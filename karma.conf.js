b237d92460b428ea22861d185dbc53b27cee030e
Steve O'Brien <steve@newicon.net>
2015-03-04 21:09:40 +0000

    Andalay first commit
    Signed-off-by: Steve O'Brien <steve@newicon.net>

---
+++ karma.conf.js
@@ -0,0 +1,84 @@
+// Karma configuration
+// Generated on Wed Mar 04 2015 14:38:24 GMT+0000 (GMT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+    plugins: [ 'karma-jasmine', 'karma-chrome-launcher', 'karma-coverage', 'karma-chrome-launcher'],
+
+    // list of files / patterns to load in the browser
+    files: [
+        // Dependencies
+        "bower_components/underscore/underscore.js",
+        "node_modules/jasmine-expect/dist/jasmine-matchers.js",
+        "bower_components/angular/angular.js",
+        "bower_components/angular-mocks/angular-mocks.js",
+        "bower_components/angular-underscore-module/angular-underscore-module.js",
+
+        // Source
+        "src/andalay.js",
+
+        // Tests
+        "test/**/*.spec.js"
+    ],
+
+
+    // list of files to exclude
+    exclude: [],
+
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors:{
+        'src/andalay.js': ['coverage']
+    },
+
+	coverageReporter: {
+		type : 'html',
+		dir : 'coverage/'
+	},
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress' 
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
